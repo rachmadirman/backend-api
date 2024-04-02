1d08fa944601d1b8066e3fa6afe0ba6605e97eaa
Fahrul Rozi <mfahrul.rozi@izeno.com>
2024-04-02 11:26:28 +0700

    add jsonproperty

--- src/main/java/com/izeno/backendapi/entity/BatchDetailTable.java
+++ src/main/java/com/izeno/backendapi/entity/BatchDetailTable.java
@@ -1,14 +1,22 @@
 package com.izeno.backendapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @Data
+@JsonPropertyOrder({"batch_id", "einvoice_number", "einvoice_document", "request_date", "validation_status", "reference_id"})
 public class BatchDetailTable {
-
+    @JsonProperty("einvoice_number")
     private String einvoicenumber;
+    @JsonProperty("einvoice_document")
     private String einvoicedocument;
+    @JsonProperty("request_date")
     private String requestdate;
+    @JsonProperty("validation_status")
     private String validationstatus;
+    @JsonProperty("batch_id")
     private String batchid;
+    @JsonProperty("reference_id")
     private String uuid;
 }
--- src/main/java/com/izeno/backendapi/entity/BatchTable.java
+++ src/main/java/com/izeno/backendapi/entity/BatchTable.java
@@ -1,14 +1,23 @@
 package com.izeno.backendapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @Data
+@JsonPropertyOrder({"batch_id", "start_date", "end_date", "status", "csv_filename", "submission_uuid"})
 public class BatchTable {
 
+    @JsonProperty("batch_id")
     private String batchid;
+    @JsonProperty("csv_filename")
     private String csvfilename;
+    @JsonProperty("start_date")
     private String start_date;
+    @JsonProperty("end_date")
     private String end_date;
+    @JsonProperty("status")
     private String status;
+    @JsonProperty("submission_uuid")
     private String submissionuuid;
 }
